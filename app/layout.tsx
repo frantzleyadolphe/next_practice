ce30658f031b7a4685688a7604b2b7b13aba8104
Adolphe Frantzley <83029225+frantzleyadolphe@users.noreply.github.com>
2024-09-22 01:18:32 -0400

    nav menu from shadcn added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
+import Header from "@/components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,11 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
-          <p>Header</p>
-          <Link href="/login">Login page</Link>
-          <Link href="/categories/1">Categories page 1</Link>
-        </div>
+        <Header />
         {children}
         <footer>Footer</footer>
       </body>
--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="p-4 border-p">
       <div className="mx-auto w-full max-w-7xl">
-        <h1>Header</h1>
+        <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-blue-600">
+          Next Practice
+        </h1>
         {/* Navigation shadcn */}
 
         {/* Buttons */}
