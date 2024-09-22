9dd6bfbd590b2d3be11aa6a2a80f773099b46119
Adolphe Frantzley <83029225+frantzleyadolphe@users.noreply.github.com>
2024-09-22 01:02:25 -0400

    header component created

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="p-4 border-p">
+      <div className="mx-auto w-full max-w-7xl">
+        <h1>Header</h1>
+        {/* Navigation shadcn */}
+
+        {/* Buttons */}
+      </div>
+    </header>
+  );
+}
